0d4b1088750b5cc802f64dad2c0087d8af2ab434
crux <cruxdevy@gmail.com>
2024-12-15 22:36:11 +0530

    refactor

--- include/vector.h
+++ include/vector.h
@@ -12,4 +12,8 @@ typedef struct {
     float z; 
 } vec3_t;
 
+vec3_t get_rotated_point(vec3_t point, vec3_t rotation); 
+vec3_t get_rotated_point_z(vec3_t point, vec3_t rotation); 
+vec3_t get_rotated_point_y(vec3_t point, vec3_t rotation); 
+vec3_t get_rotated_point_x(vec3_t point, vec3_t rotation); 
 #endif 
--- src/main.c
+++ src/main.c
@@ -37,50 +37,6 @@ void create_point_cloud(void) {
     }
 }
 
-/*
-MATH behind rotation: 
-double angle formulas of COS and SIN 
-idea of rotating in 3D: lock the axis you are rotating around(dont change anything in that axis) 
-and consider the remaining 2 axis as relative X and Y axis and do 2D rotation using a double angle formulas.
-*/
-vec3_t get_rotated_point_z(vec3_t point, vec3_t rotation) {
-    vec3_t rotated_z_point = {
-        .x=point.x * cos(rotation.z) - point.y * sin(rotation.z),
-        .y=point.x * sin(rotation.z) + point.y * cos(rotation.z),
-        .z=point.z
-    }; 
-
-    return rotated_z_point;
-}
-
-vec3_t get_rotated_point_y(vec3_t point, vec3_t rotation) {
-    vec3_t rotated_y_point = {
-        .x=point.x * cos(rotation.y) - point.z * sin(rotation.y),
-        .z=point.x * sin(rotation.y) + point.z * cos(rotation.y),
-        .y=point.y
-    }; 
-
-    return rotated_y_point;
-}
-
-vec3_t get_rotated_point_x(vec3_t point, vec3_t rotation) {
-    vec3_t rotated_x_point = {
-        .z=point.z * cos(rotation.x) - point.y * sin(rotation.x),
-        .y=point.z * sin(rotation.x) + point.y * cos(rotation.x),
-        .x=point.x
-    }; 
-
-    return rotated_x_point;
-}
-
-vec3_t get_rotated_point(vec3_t point, vec3_t rotation) {
-    // apply rotation in all three axis 
-    vec3_t rotated_point = get_rotated_point_z(point, rotation); 
-    rotated_point = get_rotated_point_y(rotated_point, rotation); 
-    rotated_point = get_rotated_point_x(rotated_point, rotation);
-    return rotated_point; 
-}
-
 void handle_input(void) {
     SDL_Event event;
     SDL_PollEvent(&event);
